594fc33b7a508b24ed18b5d16d193e95d68834a2
x1ibns <39716903+x1ibns@users.noreply.github.com>
2020-08-15 22:23:43 -0300

    Add files via upload

---
+++ aula_javascript-master/js/main.js
@@ -0,0 +1,22 @@
+/*var nome = prompt('Digite seu nome : ')
+
+alert('Bem-vindo '+nome);
+
+var idade = prompt('Digite sua idade : ');
+
+if (idade >= 18 ){
+    alert(nome+' Você é maior de idade .');
+}
+else{
+    alert(nome+' Você é menor de idade ainda.');
+}
+*/
+/*var count
+alert('contando de 0 até 5')
+for (count=0;count<=5; count++){
+    alert(count)
+}*/
+
+function clicou(){
+    alert('obrigado por clicar ')
+}
